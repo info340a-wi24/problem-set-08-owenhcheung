ebf58eb8a695849d76915f232142542bed9b4e96
Owen Cheung <ocheung198@gmail.com>
2024-02-29 20:56:32 -0800

    finish

--- problem-a/src/components/App.js
+++ problem-a/src/components/App.js
@@ -1,38 +1,54 @@
-import React from 'react'; //import React Component
-import { AboutNav } from './Nav';
-import PetPage from './PetPage';
-import PetList from './PetList';
-import AboutPage from './AboutPage';
-import ResourcesPage from './ResourcesPage';
-import PetDetail from './PetDetail';
+import React from "react"; //import React Component
+import { AboutNav } from "./Nav";
+import PetPage from "./PetPage";
+import PetList from "./PetList";
+import AboutPage from "./AboutPage";
+import ResourcesPage from "./ResourcesPage";
+import PetDetail from "./PetDetail";
 
-import SAMPLE_DOGS from '../data/dogs.json'; //a sample list of dogs (model)
+import { Routes, Route, Navigate, Link } from "react-router-dom";
+
+import SAMPLE_DOGS from "../data/dogs.json"; //a sample list of dogs (model)
 
 function App(props) {
-
   const pets = SAMPLE_DOGS; //pretend this was loaded externally or via prop
 
   return (
     <div>
       <header className="jumbotron py-4 mb-4">
         <div className="container">
-          <h1>Adopt a Pet</h1>
+          <h1>
+            <Link to="/adopt">Adopt a Pet</Link>
+          </h1>
         </div>
       </header>
-    
+
       <main className="container">
         <div className="row">
           <div className="col-3">
             <AboutNav />
           </div>
           <div className="col">
-            <PetList pets={pets} />
+            <Routes>
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/resources" element={<ResourcesPage />} />
+              <Route path="/adopt" element={<PetPage pets={pets} />}>
+                <Route path=":petName" element={<PetDetail />} />
+                <Route index element={<PetList pets={pets} />} />
+              </Route>
+              <Route path="*" element={<Navigate to="/adopt" />} />
+            </Routes>
           </div>
         </div>
       </main>
 
       <footer className="container">
-        <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+        <small>
+          Images from{" "}
+          <a href="http://www.seattlehumane.org/adoption/dogs">
+            Seattle Humane Society
+          </a>
+        </small>
       </footer>
     </div>
   );
--- problem-a/src/components/PetList.js
+++ problem-a/src/components/PetList.js
@@ -1,31 +1,37 @@
-import React from 'react'; //import React Component
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function PetList(props) {
-  let pets = props.pets || []; //handle if not provided a prop
+  let pets = props.pets || [];
   let petCards = pets.map((pet) => {
     return <PetCard key={pet.name} pet={pet} />;
-  })
+  });
 
   return (
     <div>
       <h2>Dogs for Adoption</h2>
-      <div className="row">
-          {petCards}
-      </div>
+      <div className="row">{petCards}</div>
     </div>
   );
 }
 
 function PetCard(props) {
-  let pet = props.pet; //shortcut
+  let pet = props.pet;
+
+  let petLink = `/adopt/${encodeURIComponent(pet.name)}`;
+
   return (
     <div className="col-4 flex-grow-1">
       <div className="card mb-3">
         <img className="card-img-top" src={pet.images[0]} alt={pet.name} />
         <div className="card-body">
           <h3 className="card-title">{pet.name}</h3>
-          <p className="card-text">{pet.sex} {pet.breed}</p>
-          <button className="btn btn-primary">Meet {pet.name}</button>
+          <p className="card-text">
+            {pet.sex} {pet.breed}
+          </p>
+          <Link to={petLink} className="btn btn-primary">
+            Meet {pet.name}
+          </Link>
         </div>
       </div>
     </div>
